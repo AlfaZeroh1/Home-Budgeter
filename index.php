2abbbf5dc113584849723be574a734b62b76002b
AlfaZeroh1 <alphaxardmwangi4@gmail.com>
2023-08-31 09:34:51 +0300

    Enough for today

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 include "php/DB.php";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <div class="cont">
+        <table>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Unit Price</th>
+                <th>Room</th>
+                <th>Phase</th>
+                <th>Priority</th>
+                <th>Ttl Price</th>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
